c060c1e920428bd3f8e97e46904a5a759720b193
smile <2417599488@qq.com>
2018-12-25 17:14:42 +0800

    去掉无用的reoute

--- routes/channels.php
+++ routes/channels.php
@@ -14,7 +14,3 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-
-Broadcast::channel('news', function ($user, $id) {
-    return true;
-});
--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Events\News;
 use Illuminate\Foundation\Inspiring;
 
 /*
@@ -17,9 +16,3 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
-
-
-Artisan::command('bignews', function () {
-    broadcast(new News(date('Y-m-d h:i:s A').": BIG NEWS!"));
-    $this->comment("news sent");
-})->describe('Send news');
