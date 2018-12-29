52fc77151aa5d1133680d0c1a111cbd1180fa536
luotao <luotao@gutou.com>
2018-12-29 11:10:10 +0800

    重新生成数据库填充文件

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(RolePermissionsTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
         $this->call(RolesPermissionsTableSeeder::class);
     }
