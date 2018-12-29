53eaf4a217a54612cbf37985f6d8a4f7e8fa2f36
smile <2417599488@qq.com>
2018-12-29 20:26:30 +0800

    调整代码格式

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,12 @@ $router->get('/system/health', function () {
 |
 */
 // 登录
-$router->post('admin/auth/login', 'Admin\AuthController@login')->name('admin.auth.login');
+$router->post('admin/auth/login', 'Admin\AuthController@login')
+    ->name('admin.auth.login');
 
 // 刷新token
-$router->post('admin/auth/refresh', 'Admin\AuthController@refresh')->name('admin.auth.refresh');
+$router->post('admin/auth/refresh', 'Admin\AuthController@refresh')
+    ->name('admin.auth.refresh');
 
 $router->group([
     'middleware' => 'auth:admin_api'
@@ -38,26 +40,32 @@ $router->group([
     $router->apiResource('admin', 'Admin\UserController');
     
     // 权限批量启用
-    $router->post('admin/_bulk/batchEnabled', 'Admin\UserController@batchEnable')->name('admin.batchEnable');
+    $router->post('admin/_bulk/batchEnabled', 'Admin\UserController@batchEnable')
+           ->name('admin.batchEnable');
     
     // 权限批量禁用
-    $router->post('admin/_bulk/batchDisabled', 'Admin\UserController@batchDisabled')->name('admin.batchDisabled');
+    $router->post('admin/_bulk/batchDisabled', 'Admin\UserController@batchDisabled')
+           ->name('admin.batchDisabled');
     
     // 分配角色
-    $router->post('admin/{uid}/roles', 'Admin\UserController@allotRole')->name('admin.allotRole');
+    $router->post('admin/{uid}/roles', 'Admin\UserController@allotRole')
+           ->name('admin.allotRole');
     
     // 角色查询
-    $router->get('admin/{uid}/roles', 'Admin\UserController@getRoleByRoleId')->name('admin.getRoleByRoleId');
+    $router->get('admin/{uid}/roles', 'Admin\UserController@getRoleByRoleId')
+           ->name('admin.getRoleByRoleId');
     
     // 认证
     $router->group([
         'prefix' => 'admin/auth'
     ], function (Router $router) {
         // 退出
-        $router->post('logout', 'Admin\AuthController@logout')->name('admin.auth.logout');
+        $router->post('logout', 'Admin\AuthController@logout')
+               ->name('admin.auth.logout');
         
         // 用户信息
-        $router->get('user', 'Admin\AuthController@user')->name('admin.auth.user');
+        $router->get('user', 'Admin\AuthController@user')
+               ->name('admin.auth.user');
     });
 });
 
@@ -73,13 +81,16 @@ $router->group([
     'middleware' => 'auth:admin_api'
 ], function (Router $router) {
     // 所有前端权限
-    $router->get('permission/frontend/path', 'Rbac\Permission\Permission@theFrontEndPath')->name('permission.theFrontEndPath');
+    $router->get('permission/frontend/path', 'Rbac\Permission\Permission@theFrontEndPath')
+           ->name('permission.theFrontEndPath');
     
     // 权限批量启用
-    $router->post('permission/_bulk/batchEnable', 'Rbac\Permission\Permission@batchEnable')->name('permission.batchEnable');
+    $router->post('permission/_bulk/batchEnable', 'Rbac\Permission\Permission@batchEnable')
+           ->name('permission.batchEnable');
     
     // 权限批量禁用
-    $router->post('permission/_bulk/batchDisabled', 'Rbac\Permission\Permission@batchDisabled')->name('permission.batchDisabled');
+    $router->post('permission/_bulk/batchDisabled', 'Rbac\Permission\Permission@batchDisabled')
+           ->name('permission.batchDisabled');
     
     // 权限rest资源增删改查路由
     $router->apiResource('permission', 'Rbac\Permission\Permission');
@@ -98,17 +109,21 @@ $router->group([
     'middleware' => 'auth:admin_api'
 ], function (Router $router) {
     // 角色批量启用
-    $router->post('roles/_bulk/batchEnable', 'Rbac\Role\Role@batchEnable')->name('roles.batchEnable');
+    $router->post('roles/_bulk/batchEnable', 'Rbac\Role\Role@batchEnable')
+           ->name('roles.batchEnable');
     
     // 角色批量禁用
-    $router->post('roles/_bulk/batchDisabled', 'Rbac\Role\Role@batchDisabled')->name('roles.batchDisabled');
+    $router->post('roles/_bulk/batchDisabled', 'Rbac\Role\Role@batchDisabled')
+           ->name('roles.batchDisabled');
     
     // 角色rest资源增删改查路由
     $router->apiResource('roles', 'Rbac\Role\Role');
     
     // 角色权限分配
-    $router->post('roles/{roleid}/permission', 'Rbac\Role\Role@allotPermission')->name('roles.allotPermission');
+    $router->post('roles/{roleid}/permission', 'Rbac\Role\Role@allotPermission')
+           ->name('roles.allotPermission');
     
     // 角色权限查询
-    $router->get('roles/{roleid}/permission', 'Rbac\Role\Role@getPermissionByRoleId')->name('roles.getPermissionByRoleId');
+    $router->get('roles/{roleid}/permission', 'Rbac\Role\Role@getPermissionByRoleId')
+           ->name('roles.getPermissionByRoleId');
 });
