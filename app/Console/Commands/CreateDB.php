77bdec10f2ec72b80582961ccf0cf33679db7a33
smile <2417599488@qq.com>
2019-01-24 21:17:15 +0800

    新增创建数据库的命令

---
+++ app/Console/Commands/CreateDB.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateDB extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'base-admin:create-db
+                                                {dbname : 请输入数据库名称}';
+    
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '快速创建数据库';
+    
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $schemaName = $this->argument('dbname') ? : config('database.connections.mysql.database');
+        $charset    = config('database.connections.mysql.charset', 'utf8mb4');
+        $collation  = config('database.connections.mysql.collation', 'utf8mb4_unicode_ci');
+        
+        $query = "CREATE DATABASE IF NOT EXISTS $schemaName CHARACTER SET $charset COLLATE $collation;";
+        
+        DB::statement($query);
+    
+        $this->info('数据库创建成功');
+    }
+}
--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -30,13 +30,10 @@ class Install extends Command
     {
         set_time_limit(120);
         
-        // 1.替换数据库信息
+        // 替换数据库信息
         $host = $this->ask('What is your mysql host?', config('database.connections.mysql.host'));
         
         $port = $this->ask('What is your port of mysql?', config('database.connections.mysql.port', '3306'));
-        
-        $database = $this->ask('What is your database?',
-            config('database.connections.mysql.database'));
         
         $user = $this->ask('What is your username of mysql?', config('database.connections.mysql.username'));
         
@@ -47,45 +44,51 @@ class Install extends Command
         file_put_contents($path, str_replace([
             'DB_HOST=' . config('database.connections.mysql.host'),
             'DB_PORT=' . config('database.connections.mysql.port'),
-            'DB_DATABASE=' . config('database.connections.mysql.database'),
             'DB_USERNAME=' . config('database.connections.mysql.username'),
             'DB_PASSWORD=' . config('database.connections.mysql.password'),
         ], [
             'DB_HOST=' . $host,
             'DB_PORT=' . $port,
-            'DB_DATABASE=' . $database,
             'DB_USERNAME=' . $user,
             'DB_PASSWORD=' . $pass
         ], file_get_contents($path)));
-        $this->info('db config Successful setup.');
-    
-        // 2.生成app key
+        $this->info('保存设置成功.');
+        
+        // 创建数据库
         $this->info('');
-        $this->info('app key is being generated...');
+        $this->info('正在创建数据库...');
+        Artisan::call('base-admin:create-db', [
+            'dbname' => config('database.connections.mysql.database')
+        ]);
+        $this->info('数据库创建成功.');
+        
+        // 生成app key
+        $this->info('');
+        $this->info('正在生成app key...');
         Artisan::call('config:cache');
         Artisan::call('key:generate');
-        $this->info('app key Successful setup.');
-    
-        // 3.生成jwt secret
+        $this->info('app keys生成成功.');
+        
+        // 生成jwt secret
         $this->info('');
-        $this->info('jwt secret is being generated...');
+        $this->info('正在生成jwt secret...');
         Artisan::call('jwt:secret', ['--force' => true]);
-        $this->info('jwt secret Successful setup.');
+        $this->info('jwt secret生成成功.');
         
-        // 4.迁移表结构
+        // 迁移表结构
         $this->info('');
-        $this->info('performing migration...');
+        $this->info('执行表迁移...');
         Artisan::call('migrate');
-        $this->info('database migration succeeded');
+        $this->info('表迁移成功.');
         
-        // 5.填充数据
+        // 填充数据
         $this->info('');
-        $this->info('filling data...');
+        $this->info('执行填充数据...');
         Artisan::call('db:seed');
-        $this->info('data is successfully populated...');
-    
+        $this->info('数据填充成功...');
+        
         $this->info('');
-        $this->info('It\'s been installed successfully. Enjoy it.');
+        $this->info('base-admin-api安装成功');
     }
     
     /**
