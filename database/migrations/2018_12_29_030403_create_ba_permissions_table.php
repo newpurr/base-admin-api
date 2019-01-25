4d75c63311f8c6523da962afdc4903f178632c7d
luotao <luotao@gutou.com>
2019-01-25 11:11:00 +0800

    调整migrations

--- database/migrations/2018_12_29_030403_create_ba_permissions_table.php
+++ database/migrations/2018_12_29_030403_create_ba_permissions_table.php
@@ -21,7 +21,7 @@ class CreateBaPermissionsTable extends Migration {
 			$table->string('description')->default('')->comment('描述');
 			$table->smallInteger('permission_type')->unsigned()->comment('权限类型 1-API 2-菜单/页面 3-按钮');
 			$table->integer('parent_id')->unsigned()->comment('父级权限ID');
-			$table->boolean('state')->default(2)->comment('启用状态 1-启用 2-禁用');
+			$table->boolean('state')->default(1)->comment('启用状态 1-启用 2-禁用');
 			$table->boolean('is_deleted')->default(0)->comment('是否删除:0-未删除 1-已删除');
 			$table->timestamps();
 		});
